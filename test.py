0c3ec618e9526dd2b475992ccdc76f3505857f28
Thomas <johanns.thomas@gmail.com>
2023-04-09 17:13:46 +0200

    Delete test files at project root

--- test.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from dataclasses import dataclass
-from enum import Enum, EnumMeta
-
-from serialroomba.controllers.mode import Mode
-
-te = Mode.FULL
-print(te.name)
