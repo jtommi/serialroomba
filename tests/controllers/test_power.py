4f0fdc8fb69711e6f9f40e315db8638cbbb5798e
Thomas <johanns.thomas@gmail.com>
2023-04-09 21:23:41 +0200

    Power tests

---
+++ tests/controllers/test_power.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+from unittest.mock import Mock, patch
+
+from serialroomba.controllers.sensors.power import ChargingState, PowerController
+from serialroomba.serialroomba import SerialRoomba
+
+
+# @patch("serialroomba.controllers.controller.Controller.serial_controller")
+class TestPowerController(TestCase):
+    def test_getters(self):
+        serial_controller_mock = Mock()
+        serial_controller_mock.get_sensor_data.return_value = 1
+        power_controller = PowerController(serial_controller_mock)
+
+        self.assertEqual(power_controller.battery_capacity_mAh, 1)
+        self.assertEqual(power_controller.battery_charge_mAh, 1)
+        self.assertEqual(power_controller.battery_current_mA, 1)
+        self.assertEqual(power_controller.battery_temperature_deg_C, 1)
+        self.assertEqual(power_controller.battery_voltage_mV, 1)
+        self.assertEqual(
+            power_controller.battery_charging_state,
+            ChargingState.RECONDITIONING_CHARGING,
+        )
+        self.assertEqual(power_controller.battery_is_charging, True)
+
+        serial_controller_mock.get_sensor_data.return_value = 0
+        self.assertEqual(power_controller.battery_is_charging, False)
