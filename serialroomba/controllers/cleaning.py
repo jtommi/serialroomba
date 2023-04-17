b97e035077fbbefcdd58deb2af3510d700679946
Thomas <johanns.thomas@gmail.com>
2023-04-17 22:18:18 +0200

    Added sensors

--- serialroomba/controllers/cleaning.py
+++ serialroomba/controllers/cleaning.py
@@ -1,6 +1,8 @@
 from .controller import Controller
 from .models.command import Command, CommandEnum
+from .models.sensor import Sensor, SensorEnum
 from .models.state import State, StateEnum
+from .serial import DataTypes
 
 
 class CleaningMode(CommandEnum):
@@ -13,6 +15,16 @@ class CleaningModeState(StateEnum):
     DEFAULT = State("Default", 135)
     MAX = State("Max", 136)
     SPOT = State("Spot", 134)
+
+
+class CleaningSensor(SensorEnum):
+    DIRT_DETECT = Sensor("Dirt detect", 15, DataTypes.UNSIGNED_BYTE)
+    MAIN_BRUSH_MOTOR_CURRENT = Sensor(
+        "Main brush motor current", 56, DataTypes.SIGNED_TWO_BYTES
+    )
+    SIDE_BRUSH_MOTOR_CURRENT = Sensor(
+        "Side brush motor current", 57, DataTypes.SIGNED_TWO_BYTES
+    )
 
 
 class CleaningMotorCommand(CommandEnum):
@@ -34,3 +46,15 @@ class CleaningController(Controller):
             cleaning_mode.serial_command
         )
         self.serial_controller.send_command(cleaning_mode.serial_command)
+
+    @property
+    def dirt_detect_level(self) -> int:
+        return self.get_sensor_data(CleaningSensor.DIRT_DETECT)
+
+    @property
+    def main_brush_current_mA(self) -> int:
+        return self.get_sensor_data(CleaningSensor.MAIN_BRUSH_MOTOR_CURRENT)
+
+    @property
+    def side_brush_current_mA(self) -> int:
+        return self.get_sensor_data(CleaningSensor.SIDE_BRUSH_MOTOR_CURRENT)
--- tests/controllers/test_cleaning.py
+++ tests/controllers/test_cleaning.py
@@ -18,3 +18,11 @@ class TestCleaningController(TestCase):
         self.serial_mock.get_sensor_data.return_value = b"1"
         _ = self.cleaning_controller.current_cleaning_mode
         self.serial_mock.get_sensor_data.assert_not_called()
+
+    @patch("serialroomba.controllers.cleaning.CleaningController.get_sensor_data")
+    def test_getters(self, mock_get_sensor_data):
+        mock_get_sensor_data.return_value = 1
+
+        self.assertEqual(self.cleaning_controller.dirt_detect_level, 1)
+        self.assertEqual(self.cleaning_controller.main_brush_current_mA, 1)
+        self.assertEqual(self.cleaning_controller.side_brush_current_mA, 1)
